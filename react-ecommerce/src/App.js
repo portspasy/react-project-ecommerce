2f27764d0c3f53166353ba208991e6bb5d892f00
Taulant Culi <t.culi91@gmail.com>
2020-01-28 10:32:44 +0100

    adding hard code homepage content

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./components/homepage/homepage.component";
+
+
 function App() {
-  return <div>ReactJs Ecommerce Project!</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
