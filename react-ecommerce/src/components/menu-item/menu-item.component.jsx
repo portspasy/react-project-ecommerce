bb74b5d2a668eea18f96722ebacdd2e8ae328a4a
Taulant Culi <t.culi91@gmail.com>
2020-01-28 15:54:33 +0100

    adding hover effect

--- react-ecommerce/src/components/menu-item/menu-item.component.jsx
+++ react-ecommerce/src/components/menu-item/menu-item.component.jsx
@@ -4,14 +4,15 @@ import "./menu-item.style.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => (
   // the way we can add backgourd images in react!
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
