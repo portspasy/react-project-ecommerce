70b580a47bda66f9ac37d6e40605b7198569c66e
Taulant Culi <t.culi91@gmail.com>
2020-01-28 17:38:13 +0100

    adding Router to the project

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+/// this is just a test 
+const TestRouter = () => {
+  return (
+    <div>
+       <h1>This is TestRouter inside page!</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/test" component={TestRouter} />
     </div>
   );
 }
