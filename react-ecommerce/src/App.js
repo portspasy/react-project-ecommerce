19f41e63d0eeb4522c3e3e3bc80b03d49768ebfc
Taulant Culi <t.culi91@gmail.com>
2020-01-29 12:09:59 +0100

    adding a shop page component

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -4,6 +4,7 @@ import { Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shop/shop.component";
 
 /// this is just a test
 const TestRouter = props => {
@@ -20,6 +21,7 @@ function App() {
     <div>
       <Route exact path="/" component={HomePage} />
       <Route path="/test" component={TestRouter} />
+      <Route path="/hats" component={Shop} />
     </div>
   );
 }
---
+++ react-ecommerce/src/pages/shop/shop.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./shop.style.scss";
+
+const Shop = () => (
+  <div className="shop">
+        <h1>This is shop page</h1>
+  </div>
+);
+
+export default Shop;
