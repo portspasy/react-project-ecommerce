ca05a481e34fd1ed3df175c9b322fdc22daf3e1c
Taulant Culi <t.culi91@gmail.com>
2020-01-30 15:20:20 +0100

    creating header links and adding to main page

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 /// this is just a test
 const TestRouter = props => {
@@ -19,9 +20,12 @@ const TestRouter = props => {
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/test" component={TestRouter} />
-      <Route path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/test" component={TestRouter} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- react-ecommerce/src/pages/homepage/homepage.component.jsx
+++ react-ecommerce/src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import Header from "../../components/header/header.component"
 import Directory from "../../components/directory/directory.component";
 
 import "./homepage.style.scss";
 
 const HomePage = () => (
   <div className="homepage">
-      <Header />
       <Directory />
   </div>
 );
