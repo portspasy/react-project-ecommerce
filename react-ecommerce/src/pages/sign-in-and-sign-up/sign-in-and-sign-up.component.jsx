47dcd3f8f53dcb777cee6c941642bcf8944e30e7
Taulant Culi <t.culi91@gmail.com>
2020-01-30 17:58:56 +0100

    creating SignIn-SignUp Page component

---
+++ react-ecommerce/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.style.scss";
+
+const SignInSignUpPage = () => (
+  <div>
+    <h1>Sign In Sign Up Page</h1>
+  </div>
+);
+
+export default SignInSignUpPage;
