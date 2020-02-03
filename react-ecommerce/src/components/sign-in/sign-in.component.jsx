f8fa1b352c5b97935e35cfa9832eec6c4d4ae8b2
Taulant Culi <t.culi91@gmail.com>
2020-02-03 11:29:36 +0100

    adding and event on form submit

--- react-ecommerce/src/components/sign-in/sign-in.component.jsx
+++ react-ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -8,17 +8,31 @@ class SignIn extends Component {
       password: ""
     };
   }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState = { email: "", password: "" };
+  };
+
   render() {
     return (
       <div className="sing-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password.</span>
 
-        <form>
-          <label for="email">Email</label>
-          <input name="email" type="email" value={this.state.email} required></input>
-          <label for="password">Password</label>
-          <input name="password" type="password" value={this.state.password} required></input>
+        <form onSubmit={this.handleSubmit}>
+          <label>Email</label>
+          <input name="email" type="email" value={this.state.email} required />
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            required
+          />
+
+          <input type="submit" value="Submit Form" />
         </form>
       </div>
     );
