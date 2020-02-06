072cf73eb782e0d4093fef6c329a2775404d233f
Taulant Culi <t.culi91@gmail.com>
2020-02-06 14:49:54 +0100

    configuration of firebase

--- react-ecommerce/src/firebase/firepase.utils.js
+++ react-ecommerce/src/firebase/firepase.utils.js
@@ -1,1 +1,18 @@
-import firebase from "firebase/app"
+import firebase from "firebase/app";
+
+// for the database
+import "firebase/firestore";
+// for the authentication
+import "firebase/auth";
+
+const config = {
+ 
+};
+
+// to initialize the firebase
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+
