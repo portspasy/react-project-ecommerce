72667731cd9ce8c125109ef3bf38ee34dd162f4e
Taulant Culi <t.culi91@gmail.com>
2020-02-11 15:36:49 +0100

    adding store into our index.js Provider

--- react-ecommerce/src/index.js
+++ react-ecommerce/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+// for Redux
+// Provider: makes the Redux store available to any nested components that have been wrapped in the connect() function.
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
