4890d682edb4d05c97764f6dd4e06ba9fb624358
Taulant Culi <t.culi91@gmail.com>
2020-02-14 15:40:50 +0100

    changing the code so we can add the cart info to our new array

--- react-ecommerce/src/components/collection-preview/collection-preview.component.jsx
+++ react-ecommerce/src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
       {items
         // filter the items by max 4 item per cathegory
         .filter((item, idx) => idx < 4)
-        .map(({id, ...otherItemProps}) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
