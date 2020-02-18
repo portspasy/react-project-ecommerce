2690759e2a8144787d8496a564bd177f8ec5ed2d
Taulant Culi <t.culi91@gmail.com>
2020-02-18 17:53:55 +0100

    on click to checkout button to route us in checkout page

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/checkout" component={CheckoutPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
