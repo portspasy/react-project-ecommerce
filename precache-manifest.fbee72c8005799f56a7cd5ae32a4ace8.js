f69bb0705587d900270122910f0f996b11580955
Taulant Culi <t.culi91@gmail.com>
2020-02-21 16:59:20 +0100

    Updates

---
+++ precache-manifest.fbee72c8005799f56a7cd5ae32a4ace8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a615e6d082aee1c80a84024d5bb770",
+    "url": "/react-project-ecommerce/index.html"
+  },
+  {
+    "revision": "63477d0c2662ed0ddc06",
+    "url": "/react-project-ecommerce/static/css/main.e000f4f5.chunk.css"
+  },
+  {
+    "revision": "4122db13a141df05cb51",
+    "url": "/react-project-ecommerce/static/js/2.adc8c237.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/react-project-ecommerce/static/js/2.adc8c237.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63477d0c2662ed0ddc06",
+    "url": "/react-project-ecommerce/static/js/main.3ffbd763.chunk.js"
+  },
+  {
+    "revision": "f9dcda07c85f241c10be",
+    "url": "/react-project-ecommerce/static/js/runtime-main.c27f782e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/react-project-ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "cfc8c76a2bddc6b95b6bd002ee99de7d",
+    "url": "/react-project-ecommerce/static/media/shopping-bag.cfc8c76a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-project-ecommerce/precache-manifest.f48d3a7db0b331ee8f51ac40a995a385.js"
+  "/react-project-ecommerce/precache-manifest.fbee72c8005799f56a7cd5ae32a4ace8.js"
 );
 
 self.addEventListener('message', (event) => {
