b405ffc4602298a04b43a6f90bd691342ab21572
Taulant Culi <t.culi91@gmail.com>
2020-02-21 16:57:38 +0100

    adding removeItemFromCart utils for decreas quantity carts on checkout

--- react-ecommerce/src/redux/cart/cart.utils.js
+++ react-ecommerce/src/redux/cart/cart.utils.js
@@ -18,3 +18,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   // if it is the one and only cart
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? // we only do decrease the cartItem quantity
+        { ...cartItem, quantity: cartItem.quantity - 1 }
+      : // we return only our cartItem without modification
+        cartItem
+  );
+};
