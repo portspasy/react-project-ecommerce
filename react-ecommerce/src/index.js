334995bbc84b49d3b3293664feec6da05ca76eb2
Taulant Culi <t.culi91@gmail.com>
2020-04-05 22:15:35 +0200

    using PersistGate to our index.js file

--- react-ecommerce/src/index.js
+++ react-ecommerce/src/index.js
@@ -4,7 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 // for Redux
 // Provider: makes the Redux store available to any nested components that have been wrapped in the connect() function.
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
@@ -12,7 +14,9 @@ import App from "./App";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
