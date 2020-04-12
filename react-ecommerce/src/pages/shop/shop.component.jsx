b6d9370e2dd31cb2f1854501f8d4b61dc5387e99
Taulant Culi <t.culi91@gmail.com>
2020-04-12 23:37:51 +0200

    adding collection to shop component and adding route to match collectionID

--- react-ecommerce/src/pages/shop/shop.component.jsx
+++ react-ecommerce/src/pages/shop/shop.component.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollecetionPage from "../collcetion/collection.component";
 
 import "./shop.style.scss";
 
 const ShopPage = ({ match }) => (
   <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollecetionPage} />
   </div>
 );
 
