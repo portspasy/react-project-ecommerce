3d36414331999eb4da7284c1135c0ccc33bc1a67
Taulant Culi <t.culi91@gmail.com>
2020-04-12 23:36:50 +0200

    creating collection component and style

---
+++ react-ecommerce/src/pages/collcetion/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { CollectionItem } from "../../components/collection-item/collection-item.component";
+
+import "./collection.style.scss";
+
+const CollecetionPage = ({ match }) => (
+  <div className="collecetion-page">
+    <h2>Collecetion Page !!!</h2>
+  </div>
+);
+
+export default CollecetionPage;
