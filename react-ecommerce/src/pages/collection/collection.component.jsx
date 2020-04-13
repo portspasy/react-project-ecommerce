fee0413c565e809cf0e0ca85fc58adfae16b83fb
Taulant Culi <t.culi91@gmail.com>
2020-04-13 14:30:54 +0200

    adding style to collection page and .map items for every collection

--- react-ecommerce/src/pages/collection/collection.component.jsx
+++ react-ecommerce/src/pages/collection/collection.component.jsx
@@ -6,11 +6,20 @@ import { selectCollection } from "../../redux/shop/shop.selectors.js";
 
 import "./collection.style.scss";
 
-const CollecetionPage = ({ collection }) => (
-  <div className="collecetion-page">
-    <h2>Collecetion Page!!!</h2>
-  </div>
-);
+const CollecetionPage = ({ collection }) => {
+  const { title, items } = collection;
+
+  return (
+    <div className="collecetion-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
