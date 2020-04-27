a6b07f1e6434a4dcf156330b6fa366fa3df00f1e
Taulant Culi <t.culi91@gmail.com>
2020-04-28 01:46:09 +0200

    updating stripe integration credit card info text

--- react-ecommerce/src/pages/checkout/checkout.component.jsx
+++ react-ecommerce/src/pages/checkout/checkout.component.jsx
@@ -37,6 +37,11 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>Total: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments
+      <br />
+      4242 4242 4242 4242 (VISA) - Exp: 01/22 - CVV: 123
+    </div>
     <StripeCheckoutButton price={total} />
   </div>
 );
