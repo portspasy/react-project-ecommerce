eeeab5c18f66b7af6ad8c3de6c3cd578e2867e15
Taulant Culi <t.culi91@gmail.com>
2020-05-05 11:55:58 +0200

    hidding logger in production and showing only in case of development

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,12 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+// in case that is development mode we do need the logger, and hide in case of production 
+if (process.env.NODE_ENV === "development") {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
