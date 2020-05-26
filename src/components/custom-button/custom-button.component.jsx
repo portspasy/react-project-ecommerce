5e7aeed462454452043abf2bedcafdd5da3239b8
Taulant Culi <t.culi91@gmail.com>
2020-05-26 17:16:10 +0200

    removing scss file that we have converted to styled components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "./custom-button.style.scss";
-
 import { CustomButtonContainer } from "./custom-button.style";
 
 const CustomButton = ({children, ...props}) => (
