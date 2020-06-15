63a2e83b3aab324ca8dcae1d1a1cf535c49ec517
Taulant Culi <t.culi91@gmail.com>
2020-06-15 11:55:17 +0200

    coverting shop component from function to class comp

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,12 +5,19 @@ import CollectionsOverview from "../../components/collections-overview/collectio
 import CollecetionPage from "../collection/collection.component";
 
 import "./shop.style.scss";
-
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollecetionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  render() {
+    const { match } = this.prop;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollecetionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
